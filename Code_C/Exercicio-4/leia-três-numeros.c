ca878df924931d55c27622014d405c8b1fa79aaa
JG <diasusb@hotmail.com>
2020-11-01 17:11:53 -0300

    Consertando a lógica

--- Code_C/Exercicio-4/leia-três-numeros.c
+++ Code_C/Exercicio-4/leia-três-numeros.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+void pausar(void) {
+  printf("\nPressione alguma tecla para continuar...");
+  getch();
+}
+
 int main(){
 	
 	float numero1, numero2, numero3, maior, meio, menor, iguais;
@@ -10,41 +16,41 @@ int main(){
 	printf("Digite o terceiro valor: \n");
 	scanf("%f", &numero3);
 
-	if(((numero1 > numero2) and (numero2 > numero3)) or (numero1 == numero2 > numero3)) {
+	if(((numero1 > numero2) && (numero2 > numero3)) || (numero1 == numero2)) {
 		maior = numero1;
 		meio = numero2;
 		menor = numero3;
-	}else if(((numero1 > numero3) and (numero3 > numero2)) or (numero1 == numero3 > numero2)) {
+	}else if(((numero1 > numero3) && (numero3 > numero2)) || (numero1 == numero3)) {
 		maior = numero1;
 		meio = numero3;
 		menor = numero2;
-	}else if(((numero2 > numero1) and (numero1 > numero3)) or (numero2 == numero1 > numero3)) {
+	}else if(((numero2 > numero1) && (numero1 > numero3)) || (numero2 == numero1)) {
 		maior = numero2;
 		meio = numero1;
 		menor = numero3;
-	}else if(((numero2 > numero3) and (numero3 > numero1)) or (numero2 == numero3 > numero1)) {
+	}else if(((numero2 > numero3) && (numero3 > numero1)) || (numero2 == numero3)) {
 		maior = numero2;
 		meio = numero3;
 		menor = numero1;
-	}else if(((numero3 > numero1) and (numero1 > numero2)) or (numero3 == numero1 > numero2)) {
+	}else if(((numero3 > numero1) && (numero1 > numero2)) || (numero3 == numero1)) {
 		maior = numero3;
 		meio = numero1;
 		menor = numero2;
-	}else if(((numero3 > numero1) and (numero2 > numero1)) or (numero3 == numero2 > numero1)) {
+	}else if(((numero3 > numero1) && (numero2 > numero1)) || (numero3 == numero2)) {
 		maior = numero3;
 		meio = numero2;
 		menor = numero1;
 	}
 	
-	if((numero1 == numero2) and (numero1 == numero3)) {
+	if((numero1 == numero2) && (numero1 == numero3)) {
 		maior = numero1;
 		iguais = maior;
-		printf("Iguais: [%f] \n", iguais);
+		printf("Todos sao iguais ha: [%f] \n", iguais);
 		exit(1);
 	}
 	    
   printf("\n Maior:....[%.2f] \n Meio:.....[%.2f] \n Menor:....[%.2f] \n", maior, meio, menor);
     
-	system("PAUSE");
+	pausar();
 	return(0);
 }
