ad724b4bc9ea85b5017861bd54376da7ba67b95d
JG <diasusb@hotmail.com>
2021-02-27 05:24:59 -0300

    Add Sorteio.py - 2021

--- Code_Python/Exercicio-15/Sorteio.py
+++ Code_Python/Exercicio-15/Sorteio.py
@@ -1,23 +1,54 @@
-# 2020
+# 2021
 import random
 
-lista = []
+lista, validarOpcao, contar, pergunta = [], str(), 0, "start"
 
-enumerar = int(input("Informe a quantidade(n° inteiro) de opcoes: "))
+print('^'*50)
+enumerar = int(input("(N° inteiro) Quantidade de opções: "))
 
-for contar in range(0, enumerar):
+while(contar < enumerar):
   opcoes = str(input(f"Digite a {contar + 1}° Opção: "))
-  if (opcoes == "") or (opcoes == " "):
-    opcoes = str(input(f"Operação Inválida...Digite a {contar + 1}° Opção: "))
-  lista.append(opcoes)
+  if (opcoes.strip(" ") == validarOpcao):
+    print("\|Essa opção já existe...!\n")
+  else:
+    validarOpcao = opcoes.strip(" ")
+    lista.append(validarOpcao)
+    contar+=1
 
-while True:
+while(contar==2):
   escolhido = random.choice(lista)
-  print(f"\nA opcao sorteada é: {escolhido} \n")
-  pergunta = int(input("Deseja sortear novamente?\nContinuar(Aperte 1) ou Sair(Aperte 0)\n_"))
-  if pergunta == 0:
+  print(' _ '*17)
+  print(f"\n\t\tA opção sorteada: {escolhido}")
+  print(' _ '*17)
+  pergunta = str(input(
+    "\nDeseja sortear novamente? Sim(s) ou Não(0)_/\n_"
+  ))
+  if pergunta=="0":
+    contar+=1
     print("\nPrograma encerrado com sucesso...")
     exit()
+print('^'*50)
+
+# 2020
+# import random
+
+# lista = []
+
+# enumerar = int(input("Informe a quantidade(n° inteiro) de opcoes: "))
+
+# for contar in range(0, enumerar):
+#   opcoes = str(input(f"Digite a {contar + 1}° Opção: "))
+#   if (opcoes == "") or (opcoes == " "):
+#     opcoes = str(input(f"Operação Inválida...Digite a {contar + 1}° Opção: "))
+#   lista.append(opcoes)
+
+# while True:
+#   escolhido = random.choice(lista)
+#   print(f"\nA opcao sorteada é: {escolhido} \n")
+#   pergunta = int(input("Deseja sortear novamente?\nContinuar(Aperte 1) ou Sair(Aperte 0)\n_"))
+#   if pergunta == 0:
+#     print("\nPrograma encerrado com sucesso...")
+#     exit()
   
 # 2019
 # import random
