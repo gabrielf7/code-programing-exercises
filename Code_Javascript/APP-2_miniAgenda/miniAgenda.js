fec19130c54071c174904cb0daae7b53f8ab5939
JG <diasusb@hotmail.com>
2021-03-14 16:54:39 -0300

    Add função tempoRelogio(): função recursiva que usa 'setTimeout' para exibir horario, garantindo mais performance.

--- Code_Javascript/APP-2_miniAgenda/miniAgenda.js
+++ Code_Javascript/APP-2_miniAgenda/miniAgenda.js
@@ -1,10 +1,28 @@
-function data_e_horario_hj() {
-  let dataHj = new Date();
-  return dataHj;
-}
+// Coments Important!
+// const mes = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro","Outubro", "Novembro", "Dezembro"];
+// const opcoes = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
-function meuRelogio(){
-  // Style em JS
+// Descobrir Data e Horario //
+const semana = [
+  "Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", 
+  "Quinta-feira", "Sexta-feira","Sábado"
+];
+const opcoesHorario = {
+  timeZone: 'America/Sao_Paulo',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: "numeric",
+};
+const opcoesData = { year: 'numeric', month: 'long', day: 'numeric' };
+// Funções //
+function data_e_horario_hj(now) {
+  let dataHj = new Date();
+  let dateFormat = new Intl.DateTimeFormat([], opcoesHorario);
+  let opcoesData = now === "datahj" ? dataHj : dateFormat.format(dataHj);
+  return opcoesData;
+}
+// Style em JS
+function styleJS() {
   document.body.style.color = "white";
   document.body.style.backgroundColor = "#03bda4";
   document.getElementById("containerdataHJ").setAttribute(
@@ -16,24 +34,22 @@ function meuRelogio(){
   document.getElementById("exibaHorario").setAttribute(
     "style", "font-family: verdana; font-size: 107px;"
   );
-
-  // Lógica da Função
-  const semana = [
-    "Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", 
-    "Quinta-feira", "Sexta-feira","Sábado"
-  ];
-  // // const mes = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro","Outubro", "Novembro", "Dezembro"];
-  const opcoes = { year: 'numeric', month: 'long', day: 'numeric' }; // // weekday: 'long',
-  let infodata = (op) => { return data_e_horario_hj().toLocaleDateString("pt-br", op) };
   document.getElementById("nomeDoHorario").innerHTML = `Horário de Brasília`;
+}
+function tempoRelogio() {
+  document.getElementById("exibaHorarioTitle").innerHTML = `
+  Horário BR - ${data_e_horario_hj("hr")}
+  `;
+  document.getElementById("exibaHorario").innerHTML = `${data_e_horario_hj("hr")}`;
+  setTimeout(tempoRelogio, 1000 );
+}
+function meuRelogio(){
+  styleJS();
+
+  let infodata = (op) => { return data_e_horario_hj("datahj").toLocaleDateString("pt-br", op) };
   document.getElementById("dataHJ").innerHTML = `
-    ${semana[data_e_horario_hj().getDay()]}, ${infodata(opcoes)}
+    ${semana[data_e_horario_hj("datahj").getDay()]}, ${infodata(opcoesData)}
   `;
 
-  setInterval(function(){
-    document.getElementById("exibaHorarioTitle").innerHTML = `
-    Horário BR - ${data_e_horario_hj().toLocaleTimeString()}
-    `;
-    document.getElementById("exibaHorario").innerHTML = `${data_e_horario_hj().toLocaleTimeString()}`;
-  }, 1000);
+  tempoRelogio();
 }
